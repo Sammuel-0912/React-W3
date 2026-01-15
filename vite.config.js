55e860ab546296221e2a2b56b8bed5818cf0eb3c
Sammuel-0912 <sam60320@gmail.com>
2026-01-15 21:11:00 +0800

    second commit

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import {fileURLToPath, URL } from 'node:url';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react-w3/',
+  base: '/React-W3/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
